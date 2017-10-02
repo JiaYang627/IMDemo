09fe5ef522db12f5ac4c80986fe2f1cb4691f352
JiaYang <JiaYang627@163.com>
2017-10-02 11:42:49 +0800

    Main底部按钮初始化

--- app/src/main/java/com/jiayang/imdemo/v/activity/MainActivity.java
+++ app/src/main/java/com/jiayang/imdemo/v/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.jiayang.imdemo.R;
 import com.jiayang.imdemo.m.component.ApiComponent;
 import com.jiayang.imdemo.p.activity.MainActivityPst;
@@ -18,7 +19,7 @@ import com.jiayang.imdemo.v.iview.ImainActivityView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends BaseActivity<MainActivityPst> implements ImainActivityView {
+public class MainActivity extends BaseActivity<MainActivityPst> implements ImainActivityView, BottomNavigationBar.OnTabSelectedListener {
 
     @BindView(R.id.tv_title)
     TextView mTvTitle;
@@ -44,6 +45,7 @@ public class MainActivity extends BaseActivity<MainActivityPst> implements Imain
         ButterKnife.bind(this);
 
         initToolBar();
+        initBottomNavigation();
     }
 
     private void initToolBar() {
@@ -51,6 +53,24 @@ public class MainActivity extends BaseActivity<MainActivityPst> implements Imain
         setSupportActionBar(mToolbar);
         mTvTitle.setText("消息");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initBottomNavigation() {
+        BottomNavigationItem conversationItem = new BottomNavigationItem(R.mipmap.conversation_selected_2, getString(R.string.main_conversation));
+        BottomNavigationItem contactItem = new BottomNavigationItem(R.mipmap.contact_selected_2, getString(R.string.main_contact));
+        BottomNavigationItem pluginItem = new BottomNavigationItem(R.mipmap.plugin_selected_2, getString(R.string.main_plugin));
+
+
+        mBottomNavigationBar.addItem(conversationItem);
+        mBottomNavigationBar.addItem(contactItem);
+        mBottomNavigationBar.addItem(pluginItem);
+
+        mBottomNavigationBar.setActiveColor(R.color.btn_normal);
+        mBottomNavigationBar.setInActiveColor(R.color.inActive);
+
+        mBottomNavigationBar.initialise();
+        mBottomNavigationBar.setTabSelectedListener(this);
+
     }
 
     /**
@@ -81,20 +101,36 @@ public class MainActivity extends BaseActivity<MainActivityPst> implements Imain
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_add_friend :
+            case R.id.menu_add_friend:
                 ToastUtils.initToast("点击了 添加好友按钮");
                 //TODO 跳转添加好友
                 break;
-            case R.id.menu_scan :
+            case R.id.menu_scan:
                 ToastUtils.initToast("点击了 扫一扫按钮");
                 break;
-            case R.id.menu_about :
+            case R.id.menu_about:
                 ToastUtils.initToast("点击了 关于我们按钮");
                 break;
-            case android.R.id.home :
+            case android.R.id.home:
                 ToastUtils.initToast("点击了 侧滑菜单按钮");
                 break;
         }
         return true;
+    }
+
+    // 底部按钮点击事件监听。
+    @Override
+    public void onTabSelected(int position) {
+
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
     }
 }
